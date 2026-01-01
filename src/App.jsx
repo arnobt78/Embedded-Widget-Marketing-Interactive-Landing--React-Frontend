2651d86f4bd7d49a3eb45e5563ef5c8090a6dad3
Arnob Mahmud <arnob_t78@yahoo.com>
2026-01-01 16:07:55 +0100

    remove: feedback table, use production widget URL

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Banner from "./components/Banner/Banner";
 import Cards from "./components/Cards/Cards";
 import Banner2 from "./components/Banner/Banner2";
 import Email from "./components/Email/Email";
-import FeedbackDashboard from "./components/Dashboard/FeedbackDashboard";
 
 const App = () => {
   return (
@@ -17,8 +16,6 @@ const App = () => {
       <Cards />
       <Banner2 />
       <Email />
-      {/* Feedback Dashboard (for admin view, move as needed) */}
-      <FeedbackDashboard />
     </main>
   );
 };
